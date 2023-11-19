8132df61d66cfff332999ac3c1af43f4addc0dd9
Christian Parsons <136206613+ChrisP30@users.noreply.github.com>
2023-11-19 17:09:42 -0500

    anothercommit

--- scripts/searchFunctionality.js
+++ scripts/searchFunctionality.js
@@ -7,17 +7,63 @@ class searchFunctionality {
 
     }
 
+    async fetchData(query) {
+        try {
+            const response = await fetch('data/data.json');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            return data; 
+        } catch (error) {
+            console.error('Error fetching data: ', error);
+        }
+    }
+
+    async searchData(query) {
+        try {
+            const data = await this.fetchData();
+            if (!data) return;
+
+            const filteredData = data.filter(item => 
+                item.name.toLowerCase().includes(query.toLowerCase())
+            );
+            const limitedData = filteredData.slice(0, 10);
+            return limitedData;
+        } catch (error) {
+            console.error('Error in searchData: ', error);
+        }
+    }
+
     searchbarTyped() {
-        this.searchbar.addEventListener('input', () => {
-            if(this.searchbar.value !== '') {
+        this.searchbar.addEventListener('input', async () => { 
+            if (this.searchbar.value !== '') {
                 console.log('value not 0')
+                this.search = this.searchbar.value;
+                this.dropDownDiv.innerHTML = ''
                 this.dropDownDiv.style.display = 'block';
+                try {
+                    this.data = await this.searchData(this.search); 
+                    if (this.data) {
+                        this.data.forEach(element => {
+                            this.item = document.createElement('li')
+                            this.item.textContent = `${element.name}`
+                            this.item.addEventListener('click', () => {
+                                window.location.href = `${element.location}`
+                            })
+                            this.dropDownDiv.appendChild(this.item)
+                        });
+                    }
+                } catch (error) {
+                    console.error('Error processing data: ', error);
+                }
             } else {
                 console.log('value 0')
                 this.dropDownDiv.style.display = 'none';
             }
-        })
+        });
     }
+    
 }
 
 new searchFunctionality();
