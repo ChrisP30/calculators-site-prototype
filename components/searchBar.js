12a2aecf9a699096039c1b238d71294859f9f5c1
Christian Parsons <136206613+ChrisP30@users.noreply.github.com>
2023-11-21 21:29:18 -0500

    Styling and adding search-bar component

---
+++ components/searchBar.js
@@ -0,0 +1,20 @@
+class searchBar extends HTMLElement {
+    constructor() {
+        super()
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <link rel="stylesheet" href="../styles/search.css">
+        <div class="searchbar-container">
+            <div>
+                <input type="text" placeholder="Search..." id="searchbar">
+                <ul id="dropdown">
+                
+                </ul>
+            </div>
+        </div>`
+    }
+}
+
+customElements.define('search-bar', searchBar)
